8d17ac004623030496f706c2a54ae8154ac41e7f
umayer10-AI <mdumayer10@gmail.com>
2026-07-12 12:28:04 +0600

    add

--- src/component/Card.tsx
+++ src/component/Card.tsx
@@ -12,7 +12,7 @@ export default function Card({ hotel }: { hotel: any }) {
   return (
     <div className="group overflow-hidden rounded-2xl border border-zinc-800 bg-zinc-900 transition-all duration-300 hover:-translate-y-1 hover:border-orange-500 hover:shadow-2xl hover:shadow-orange-500/20">
       {/* Image */}
-      <div className="relative h-60 overflow-hidden">
+      <div className="relative h-40 overflow-hidden">
         <Image
           src={hotel.image}
           alt={hotel.title}
@@ -30,7 +30,7 @@ export default function Card({ hotel }: { hotel: any }) {
       </div>
 
       {/* Content */}
-      <div className="space-y-5 p-5">
+      <div className="space-y-1 p-5">
         <div>
           <h2 className="line-clamp-1 text-xl font-bold text-white">
             {hotel.title}
@@ -72,7 +72,7 @@ export default function Card({ hotel }: { hotel: any }) {
           </div>
 
           <Link
-            href={`/hotel/${hotel.id}`}
+            href={`/hotel/${hotel._id}`}
             className="flex items-center gap-2 rounded-xl bg-gradient-to-r from-orange-600 via-orange-600 to-orange-400 px-5 py-2.5 text-sm font-semibold text-white transition-all duration-300 hover:scale-105"
           >
             View Details
